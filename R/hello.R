8d297180577d7a243dd995beba6df683d9968047
Stephan Maier <stephan_mar_maier@yahoo.co.uk>
2018-02-26 11:00:49 +0000

    =test

--- R/hello.R
+++ R/hello.R
@@ -1,9 +1,8 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
+#e about package authoring with RStudio at:
 #
 #   http://r-pkgs.had.co.nz/
 #
