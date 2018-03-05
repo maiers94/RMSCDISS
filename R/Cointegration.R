39ac6a9a645de9c4e6b13199a39ce8ac49aa0f70
Stephan Maier <stephan_mar_maier@yahoo.co.uk>
2018-03-05 15:51:23 +0000

    Euclidean Function Ready, Roxygenised

--- R/Cointegration.R
+++ R/Cointegration.R
@@ -1,12 +1,28 @@
+#'@export
 list.cointegration <- function(datalist,data,testfun){
-  index <- 0
-  for(i in 1:length(datalist)){
-    results <- rep(0,choose(length(datalist),2))
-    sec1 <- datalist[i]
-    for(j in 1:(length(datalist)-i)){
-      index <- index + 1
-      sec2 <- datalist[i+j]
-      results[index] <- testfun()
-    }
+
+  results <- rep(0,length(datalist))
+  for(i in 1:length(datalist[[1]])){
+    sec1 <- data[,datalist[[1]][i]]
+    sec2 <- data[,datalist[[2]][i]]
+    results[i] <- testfun(sec1,sec2)
   }
+  return(list(datalist[[1]],datalist[[2]],results))
+}
+
+
+#'@export
+euclidean <- function(sec1,sec2){
+  sumsq <- 0
+  for(i in 1:length(sec1)){
+    diffsq <- (sec1[i]-sec2[i])**2
+    sumsq <- sumsq + diffsq
+  }
+  return(sumsq)
+}
+
+
+johansen <- function(sec1,sec2){
+
+  return(metric)
 }
--- R/Data_Handler.R
+++ R/Data_Handler.R
@@ -12,15 +12,33 @@ pickup <- function(data="data/FTSE100.Rdata"){
   return(NULL)
 }
 
-
-cleanup <- function(data){
-  return(clean)
+price2ret <- function(data){
+  it <- ncol(data)
+  output <- matrix(nrow=nrow(data),ncol=it)
+  for(i in 1:it){
+    base <- as.numeric(data[1,i])
+    output[,i] <- as.numeric(as.vector(data[,i]))/base
+  }
+  return(output)
 }
+
 
 partition <- function(data){
   return(part)
 }
 
-listgen <- function(){
-  return(list)
+listgen.allc <- function(b){
+  v1 <- vector(length=choose(ncol(b),2))
+  v2 <- vector(length=choose(ncol(b),2))
+  nos <- ncol(b)
+  index <- 0
+  for(i in 1:(nos-1)){
+    for(j in (1+i):nos){
+      index <- index + 1
+      v1[index] <- i
+      v2[index] <- j
+    }
+  }
+  l <- list(v1,v2)
+  return(l)
 }
