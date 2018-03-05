a147761547e93f14134aa00bf5ad18ce6d932b32
Stephan Maier <stephan_mar_maier@yahoo.co.uk>
2018-03-05 11:52:13 +0000

    mend

---
+++ R/Data_Handler.R
@@ -0,0 +1,12 @@
+# Importing Algorithm
+#
+#   a <- readxl::read_excel("FTSE100.xlsx")
+#   for(i in 2:dim(a)[2]){
+#     nam <- paste("STOCK", i-1, sep = "_")
+#     assign(nam,a[2:4697,i])
+#   }
+#
+
+cleanup <- function(datalist){
+  return(clean)
+}
